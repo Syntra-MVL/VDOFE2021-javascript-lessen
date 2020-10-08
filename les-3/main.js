855bcecc8182c66780fef94b58ee21097178c059
Korneel Eeckhout <korneel@pienter.tech>
2020-10-08 10:11:18 +0200

    Rock Paper Scissors

---
+++ les-3/main.js
@@ -0,0 +1,14 @@
+const rockPaperScissors = function() {
+    // prompt 'blad', 'steen' of 'schaar'
+    // select random 'blad', 'steen' of 'schaar' voor AI
+    // determine winner
+    // return true if player wins
+    // return false if AI wins
+}
+
+const rockPaperScissorsCompetition = function () {
+    // prompt player name
+    // prompt number of rounds
+    // play correct number of round
+    // alert winner
+}
