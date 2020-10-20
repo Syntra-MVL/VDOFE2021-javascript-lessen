43b5c60756e4eafa5161e6c7b3312bead6b1f172
Korneel Eeckhout <korneel@pienter.tech>
2020-10-20 11:54:40 +0200

    DOM

---
+++ les-5/main.js
@@ -0,0 +1,24 @@
+// console.log(document);
+
+// console.log(document.childNodes[1]);
+
+const htmlTag = document.childNodes[1];
+const bodyTag = htmlTag.children[1];
+
+// console.log(htmlTag.childNodes);
+// console.log(htmlTag.children);
+
+const paragraphs = document.getElementsByClassName('paragraph');
+console.log(paragraphs);
+
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+console.log(input);
+console.log(button);
+
+const buttonClicked = function() {
+    console.log('button clicked');
+    console.log(input.value);
+}
+
+button.addEventListener('click', buttonClicked);
