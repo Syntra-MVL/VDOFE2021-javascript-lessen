a4a7464afce5f1fa469af051cd0c9e18339b32b1
Korneel Eeckhout <korneel@pienter.tech>
2020-11-09 15:36:06 +0100

    Init ex

---
+++ les-8/calculator/script.js
@@ -0,0 +1,37 @@
+let $sumSubmitBtn;
+let $sumResultContainer;
+let $productSubmitBtn;
+let $productResultContainer;
+let $numberInputContainer;
+let $addInputBtn;
+let $numberCountContainer;
+
+function sumNumbers() {
+  // return the sum value of all the numbers
+}
+
+function printSum() {
+  // print the sum value of all the numbers
+}
+
+function multiplyNumbers() {
+  // return the product of all the numbers
+}
+
+function printProduct() {
+  // print the product of all the numbers
+}
+
+function printNumberCount() {
+  // get and print the number of input fields
+}
+
+function addNumberInput() {
+  // add a number input
+}
+
+function removeNumberInput(event) {
+  // remove a number input
+}
+
+// add event listeners
