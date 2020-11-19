677484f786ec519037f4cd1c1b1be8d250df668e
Korneel Eeckhout <korneel@pienter.tech>
2020-11-19 09:57:41 +0100

    Word scrambler

--- les-8/word-scrambler/script.js
+++ les-8/word-scrambler/script.js
@@ -1,64 +1,103 @@
 //select textarea
-let userInput;
+const userInput = document.getElementById('user-input');
 //select btn
-let submitBtn;
+const submitBtn = document.getElementById('submit-btn');
 //select result container
-let resultContainer;
+const resultContainer = document.getElementById('result-container');
 //# select wordCount
-let wordCountContainer;
+const wordCountContainer = document.getElementById('word-count');
 //# select letterCount
-let letterCountContainer;
+const letterCountContainer = document.getElementById('letter-count');
 
 function getUserInput() {
   //return value of userInput
+  return userInput.value;
 }
 
 function textToWordArray(text) {
   // return array of words
-  // text.split(' ');
+  return text.split(' ');
 }
 
 function getRandomNumber(max) {
   // return random number between 0 and max (including 0 and excluding max)
+  return Math.floor(Math.random() * max);
 }
 
 function scrambleArray(oldArray) {
   // return scrambled array
+  // for (let i=0; i < oldArray.length; i++) {
+  //   const randomIndex = getRandomNumber(oldArray.length);
+  //   const tempVal = oldArray[i];
+  //
+  //   oldArray[i] = oldArray[randomIndex];
+  //   oldArray[randomIndex] = tempVal;
+  // }
+
+  // for (let i=oldArray.length - 1; i > 0; i--) {
+  //   const randomIndex = getRandomNumber(i);
+  //   const tempVal = oldArray[i];
+  //
+  //   oldArray[i] = oldArray[randomIndex];
+  //   oldArray[randomIndex] = tempVal;
+  // }
+
+  const scrambledArray = [];
+
+  while (oldArray.length > 0) {
+    const randomIndex = getRandomNumber(oldArray.length);
+    const randomVal = oldArray.splice(randomIndex, 1);
+
+    scrambledArray.push(randomVal[0]);
+  }
+
+  return scrambledArray;
 }
 
-function scrambleText(text) {
+function wordArrayToText(array) {
   // return scrambled text
-  // array.join(' ');
+  return array.join(' ');
 }
 
 function getWordCount(array) {
   //# return word count
+  return array.length;
 }
 
 function getLetterCount(text) {
   //# return letter count
+  return text.length;
 }
 
 function updateWordCount(wordCount) {
   //# update the Word Count
+  wordCountContainer.textContent = wordCount;
 }
 
 function updateLetterCount(letterCount) {
   //# update the Letter Count
+  letterCountContainer.textContent = letterCount;
 }
 
-function updateCounts(event) {
+function updateCounts(text, wordArray) {
   //# update Word Count and Letter Count
+  updateWordCount(getWordCount(wordArray));
+  updateLetterCount(getLetterCount(text));
 }
 
-function onClickScramble() {
+function scramble() {
   // update textContent of resultContainer
-}
+  const input = getUserInput();
+  const wordArray = textToWordArray(input);
+  const scrambledArray = scrambleArray(wordArray);
+  const scrambledText = wordArrayToText(scrambledArray);
 
-function realTimeScramble(event) {
-  //## update textContent of resultContainer realtime
+  updateCounts(scrambledText, scrambledArray);
+  resultContainer.textContent = scrambledText;
 }
 
 //add click event listener to submitBtn
-//# add input event listener to userInput for counts
+submitBtn.addEventListener('click', scramble);
+
 //## add input event listener to userinput for realTimeScramble
+userInput.addEventListener('input', scramble);
